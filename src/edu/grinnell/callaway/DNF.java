82e098a218b2c3470598e49c81bd2febb78f768b
Erin Callaway <callaway@grinnell.edu>
2014-03-02 16:23:20 -0600

    Cleaned up DNF and DNFTets done.

--- src/edu/grinnell/callaway/DNF.java
+++ src/edu/grinnell/callaway/DNF.java
@@ -19,42 +19,31 @@ public class DNF
    */
   public static void dnf(String[] vals, StringClassifier classifier)
   {
-    int begin =0, r=0, i=0, b=vals.length, end=vals.length;
+    int r=0, i=0, b=vals.length, end=vals.length;
    
     while(i < b)
       {
         String move = vals[i];
         if (classifier.classify(vals[i])<0)
-          {
-            
+          {          
             for (int j=i; j>r; j--)
               {
                 vals[j]= vals[j-1];
-              }
+              } // for (j)
             vals[r]=move;
             r++;
-          }
+          } // if (classifier.classify(vals[i])<0)
         else if (classifier.classify(vals[i])>0)
           {
-            for (int j=i; j<end-1; j++)
+            for (int k=i; k<end-1; k++)
               {
-                vals[j] = vals[j+1];
-              }
+                vals[k] = vals[k+1];
+              } // for (k)
             vals[end-1]= move;
             b--;
             i--;
-          }
+          } // else if (classifier.classify(vals[i])>0)
         i++;
-      }//   int x = classifier.classify(vals[0]);
+      }// while (i<b)
   } // dnf(String[], StringClassifier) 
-  
-  public static void main(String[] args)
-  {
-    String[] str = new String[]{"12345", "666666", "456", "123", "55555", "2", "1", "345", "67890"};
-    String[] th = new String[]{"This", "34", "this", "IT",};
-    dnf(str, new SimpleStringSizeClassifier());
-    dnf(th, new ClassifyStringsByFirstCharacter());
-    System.out.println(Arrays.toString(str));
-    System.out.println(Arrays.toString(th));
-  }
 } // class DNF
--- src/edu/grinnell/callaway/DNFTest.java
+++ src/edu/grinnell/callaway/DNFTest.java
@@ -2,14 +2,26 @@ package edu.grinnell.callaway;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class DNFTest
 {
-
-  public static void main(String[] args)
+  @Test
+  public void test()
   {
-   // DNF.dnf(new String[] {"this", "tssrsdfsd", "sdfsd"}, new StringClassifier());
-  }
-
-}
+    String[] str = new String[]{"12345", "666666", "456", "123", "55555", "2", "1", "345", "67890"};
+    String[] ring = new String[]{"12345", "123456", "123"};
+    String[] ing = new String[]{"This", "34", "this", "IT"};
+    DNF.dnf(str, new SimpleStringSizeClassifier());
+    DNF.dnf(ring, new SimpleStringSizeClassifier());
+    DNF.dnf(ing, new ClassifyStringsByFirstCharacter());
+    assertArrayEquals(new String[]{"456", "123", "2", "1", "345", "12345", "55555", "67890", "666666"},
+                      str);
+    assertArrayEquals(new String[]{"123", "12345", "123456"},
+                      ring);
+    assertArrayEquals(new String[]{"this", "This", "IT", "34"},
+                      ing);
+  } // test()
+} // DNFTest
