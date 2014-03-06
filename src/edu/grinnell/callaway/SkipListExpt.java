5a7b101b8b985c3ea248ea317a798f4d8bfeb912
Erin Callaway <callaway@grinnell.edu>
2014-03-05 22:25:07 -0600

    Skip lists finished.

--- src/edu/grinnell/callaway/SkipListExpt.java
+++ src/edu/grinnell/callaway/SkipListExpt.java
@@ -5,17 +5,34 @@ public class SkipListExpt
   public static void main(String[] args)
   {
 
-    SkipListOfStrings ss = new SkipListOfStrings();
-    System.out.println(ss);
+    SkipListOfStrings list = new SkipListOfStrings();
+    System.out.println(list);
 
-    ss.add("m");
-    ss.add("i");
-    ss.add("a");
+    list.add("m");
+    list.add("i");
+    list.add("a");
+    list.add("dancing");
+    list.add("i am electric");
+    list.add("banana");
+    list.add("breakfast at tiffany's");
+    list.add("1");
+    list.add("0");
+    list.add(" ");
+    list.add("xylophone");
+    list.add("xylophones");
 
-    System.out.println(ss.contains("c"));
-    System.out.println(ss);
-    ss.remove("a");
+    System.out.println("contains 'i': " + list.contains("i"));
+    System.out.println("contains '0': " + list.contains("0"));
+    System.out.println("contains 'breakfast at johns': "
+                       + list.contains("breakfast at johns"));
+    System.out.println("contains 'zebra': " + list.contains("zebra") + "\n");
 
-    System.out.println(ss);
-  }
-}
+    System.out.println(list + "\n");
+    list.remove("a");
+    list.remove("dancing");
+    System.out.println("contains 'a': " + list.contains("a"));
+    System.out.println("contains 'dancing': " + list.contains("dancing") + "\n");
+
+    System.out.println(list);
+  } // main
+} // SkipListExpt
--- src/edu/grinnell/callaway/SkipListOfStrings.java
+++ src/edu/grinnell/callaway/SkipListOfStrings.java
@@ -7,103 +7,112 @@ public class SkipListOfStrings
     implements
       SetOfStrings
 {
-
-  /*
-   * Fields and Constants
+  /**
+   * Nodes of the skip list
    */
-
-  int MAX_LEVEL = 20;
-  int level=1;
-  double p = 0.5;
-
-  Node HEAD = new Node("HEAD", MAX_LEVEL);
-  Node NIL = new Node("NIL", MAX_LEVEL);
-//  Node TEST = new Node("TEST", 20);
-
   private class Node
   {
-    /*
-     * Fields
-     */
+    public final String val;
+    public final Node[] pointer;
 
-    String val;
-    int level;
-    Node[] pointer;
-
-    /*
-     * Constructors
-     */
-    private Node(String str, int lvl)
+    public Node(String str, int lvl)
     {
-      val = str;
-      level = lvl;
-      pointer = new Node[level];
+      this.val = str;
+      pointer = new Node[lvl + 1];
     }// Node(String str, int lvl)
   }// private class Node
 
   /*
-   * Constructors
+   * Constants and Fields
    */
- /* public SkipListOfStrings()
-  {
-    this.level = 1;
-    for (int i = 0; i < MAX_LEVEL; i++)
-      {
-        this.HEAD.pointer[i] = NIL;
-  //      this.TEST.pointer[i] = NIL;
-      }// for
-  }// SkipListOfStrings()*/
+  int MAX_LEVEL = 20;
+  int level = 0;
+  double p = 0.5;
 
-  /*
-   * Methods
+  Node HEAD = new Node(null, MAX_LEVEL);
+
+  /**
+   * generates a random level from 1 to MAX_LEVEL-1, inclusive
+   * 
+   * @pre MAX_LEVEL is predefined
+   *      p is predefined
+   * 
+   * @post 0 < lvl < MAX_LEVEL
+   * 
+   * @return int lvl
    */
-
   public int level()
   {
-   int lvl = 1;
+    int lvl = 1;
     while (Math.random() < p && lvl < MAX_LEVEL)
       {
         lvl++;
-      }
-
-    System.out.println("level: " + level);
+      }// while
     return lvl;
-    
   }// level()
+
+  /**
+   * determines whether 'str' is contained within this skip list
+   * 
+   * @param String
+   *          str String to search for in this skip list
+   * 
+   * @pre str must be a valid string 
+   *      this skip list must be predefined
+   * 
+   * @return boolean
+   */
 
   public boolean contains(String str)
   {
     Node current = this.HEAD;
-    for (int i = this.level; i > 0; i--)
+    for (int i = level; i >= 0; i--)
       {
-        while (current.pointer[i] != null && str.compareTo(current.pointer[i].val) > 0)
+        while (current.pointer[i] != null
+               && current.pointer[i].val.compareTo(str) < 0)
           {
             current = current.pointer[i];
-          } // while
-      } // for
-    current = current.pointer[1];
-   
-      return (current.val != null && str.equals(current.val));
-    
-    
+          }// while
+      }// for
 
+    current = current.pointer[0];
+    return (current != null && current.val.equals(str));
   }// contains(String str)
 
+  /**
+   * adds a string 'str' to this list in alphabetical order
+   * 
+   * @param String
+   *          str
+   * 
+   * @pre str must be a valid string 
+   *      this skip list must be predefined
+   *      MAX_LEVEL is predefined
+   * 
+   * @post str is in this skip list in the correct alphabetical position
+   *       all pointers of all elements of this skip list have been
+   *       adjusted accordingly
+   *       if necessary, the level of this skip list has been increased
+   *       accordingly
+   */
   public void add(String str)
   {
     Node current = this.HEAD;
     Node[] update = new Node[MAX_LEVEL + 1];
+
     for (int i = this.level; i >= 0; i--)
       {
-       // System.out.println("i = " + i);
-        while (current.pointer[i] != null && str.compareTo(current.pointer[i].val) > 0)
+        while (current.pointer[i] != null
+               && current.pointer[i].val.compareTo(str) < 0)
           {
             current = current.pointer[i];
           }// while
         update[i] = current;
       } // for
-    current = current.pointer[1];
-    if (current == null || !str.equals(current.val))
+
+    current = current.pointer[0];
+
+    if (current == null || !current.val.equals(str))
       {
         int lvl = level();
         if (lvl > this.level)
@@ -115,24 +124,40 @@ public class SkipListOfStrings
             this.level = lvl;
           }// if
         current = new Node(str, lvl);
-        for (int i = 0; i<lvl; i++)
+        for (int i = 0; i <= lvl; i++)
           {
             current.pointer[i] = update[i].pointer[i];
             update[i].pointer[i] = current;
           }// for
-      } // if 
-
+      } // if
   }// add(String str)
+
+  /**
+   * removes a string 'str' from this skip list
+   * 
+   * @param String
+   *          str
+   * 
+   * @pre str must be a valid string 
+   *      this skip list must be predefined
+   *      MAX_LEVEL is predefined
+   * 
+   * @post str is removed from this skip list 
+   *       all pointers of all remaining elements of this skip list 
+   *       have been reassigned accordingly.
+   * 
+   */
 
   public void remove(String str)
   {
-
     Node current = this.HEAD;
     Node[] update = new Node[MAX_LEVEL + 1];
+
     for (int i = this.level; i >= 0; i--)
       {
-       // System.out.println("i = " + i);
-        while (current.pointer[i] != null && str.compareTo(current.pointer[i].val) > 0)
+        // System.out.println("i = " + i);
+        while (current.pointer[i] != null
+               && str.compareTo(current.pointer[i].val) > 0)
           {
             current = current.pointer[i];
           }// while
@@ -141,33 +166,40 @@ public class SkipListOfStrings
     current = current.pointer[0];
     if (str.equals(current.val))
       {
-        for (int i=0; i<this.level; i++)
+        for (int i = 0; i < this.level; i++)
           {
             if (update[i].pointer[i] != current)
-                break;
+              break;
             update[i].pointer[i] = current.pointer[i];
-            current = null;
-            while (this.level >1 && this.HEAD.pointer[this.level] ==NIL)
-                this.level = this.level-1;
-          }
-      }
-    
-
+            while (this.level > 0 && this.HEAD.pointer[this.level] == null)
+              this.level--;
+          }// for
+      }// if
   }// remove(String str)
+  
+  /**
+   * converts this skip list to printable format for output
+   * 
+   * @pre this skip list is predefined
+   * 
+   * credit: inspired by toString method on michaelnaper.com
+   *    (took cue from code, significantly modfied for our use)
+   * 
+   */
 
   public String toString()
   {
     StringBuilder sb = new StringBuilder();
     sb.append("{");
-    Node x = HEAD.pointer[0];
-    while (x != null)
+    Node current = HEAD.pointer[0];
+    while (current != null)
       {
-        sb.append(x.val);
-        x = x.pointer[0];
-        if (x != null)
+        sb.append(current.val);
+        current = current.pointer[0];
+        if (current != null)
           sb.append(",");
-      }
+      }// while
     sb.append("}");
     return sb.toString();
-  }
+  }// toString()
 } // class SkipListOfStrings
